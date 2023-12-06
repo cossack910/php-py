1e3871f250d9b4c5d2cb5eaa768a1e9f7e6badef
github Toshiaki Takagi <maris160zhk@gmail.com>
2023-12-07 00:54:19 +0900

    end

--- problem/ALDS1_1_A.php
+++ problem/ALDS1_1_A.php
@@ -3,10 +3,6 @@
 function insertionSort(array $A,int $N) {
     print(implode(' ', $A).PHP_EOL);
 
-    if (true === sortJudge($A, $N)) {
-        return;
-    }
-
     for ($i = 1; $i < $N; $i++) {
         $v = $A[$i];
         $j = $i - 1;
@@ -17,19 +13,6 @@ function insertionSort(array $A,int $N) {
         $A[$j+1] = $v;
         print(implode(' ', $A).PHP_EOL);
     }
-}
-
-function sortJudge(array $A, int $N) {
-    foreach($A as $key => $val){
-        if ($key + 1 === $N) {
-            break;
-        }
-
-        if ($val > $A[$key + 1]) {
-            return false;
-        }
-    }
-    return true;
 }
 
 $N = (int) trim(fgets(STDIN));
