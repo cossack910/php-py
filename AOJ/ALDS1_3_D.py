fdd71077e78ea540b34e153f8e2f371fc849e296
cossack910 <maris160zhk@gmail.com>
2023-12-19 04:11:16 +0900

    OK

--- AOJ/ALDS1_3_D.py
+++ AOJ/ALDS1_3_D.py
@@ -1,11 +1,10 @@
 mizu = input()
 
-area = 0
-mizu_arr = []
+area = []
 
+top =0
 x = 0
 y = 0
-map = [[y, x]]
 for m in range(len(mizu)):
     if mizu[m] == "_":
         x += 1
@@ -17,6 +16,3 @@ for m in range(len(mizu)):
     if mizu[m] == "/":
         x += 1
         y += 1
-        
-    map.append([y, x])
-print(map)
