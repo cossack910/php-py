a08e5135760cfdfd6b3d1c9ae80eaca459183f00
cossack910 <maris160zhk@gmail.com>
2023-12-27 08:47:24 +0900

    ...

--- AOJ/ALDS1_6/ALDS1_6_A.py
+++ AOJ/ALDS1_6/ALDS1_6_A.py
@@ -1,18 +1,18 @@
-def CountingSort(k):
-    C = [0 for _ in range(k)]
+def CountingSort():
+    C = [0 for _ in range(max(A) + 1)]
     
-    for j in range(1, len(A)):
+    for j in range(len(A)):
         C[A[j]] += 1
         
-    for i in range(1, k):
+    for i in range(1, max(A) + 1):
         C[i] = C[i] + C[i - 1]
         
-    for j in reversed(range(1, len(A))):
+    for j in reversed(range(len(A))):
         B[C[A[j]]] = A[j]
         C[A[j]] -= 1
-
+    
 k = int(input())
 A = list(map(int, input().split(' ')))
-B = ['*' for _ in range(k)]
-CountingSort(k)
-print(' '.join(list(map(str, B))))
+B = ['*' for _ in range(k+1)]
+CountingSort()
+print(' '.join(list(map(str, B[1:]))))
